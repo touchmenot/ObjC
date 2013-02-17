30c4b072ba422bdd71dfc58890dc3b9adff872e7
Balaji <balaji.sampath10@gmail.com>
2013-02-16 19:12:15 -0500

    Xcode 4+

---
+++ Chapter1/Chapter1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Chapter1
+//
+//  Created by Balaji on 2/16/13.
+//  Copyright (c) 2013 Balaji. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
